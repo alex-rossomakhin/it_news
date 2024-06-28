7c4c1cd274c5edbecc7bc2b5e666f21eeb2c9d48
Alex Ross <alexeirossomahin@yandex.ru>
2024-06-28 21:20:22 +0300

    create model

--- it_news/it_news/settings.py
+++ it_news/it_news/settings.py
@@ -53,10 +53,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'it_news.urls'
 
+TEMPLATES_DIR = BASE_DIR/ 'templates'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +129,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.MyUser'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static_dev',
+] 
--- it_news/news/models.py
+++ it_news/news/models.py
@@ -1,3 +1,51 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class News(models.Model):
+    
+    title = models.CharField(max_length=128,
+                             verbose_name='Заголовок'
+                             )
+    text = models.TextField(verbose_name='Текст новости'
+                            )
+    image = models.ImageField(blank=True,
+                              verbose_name='Изображение'
+                              )
+    slug = models.SlugField(unique=True,
+                            verbose_name='URL'
+                            )
+    pub_data = models.DateTimeField('Дата публикации',
+                                    auto_now_add=True
+                                    )
+    author = models.ForeignKey(User, 
+                               on_delete=models.CASCADE,
+                               related_name='author_news',
+                               verbose_name='Автор'
+                               )
+    class Meta:
+        ordering = ['-pub_data']
+        verbos_name = 'Новость'
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='author_comment',
+                               verbose_name='Автор комментария'
+                               )
+    news = models.ForeignKey(News,
+                             on_delete=models.CASCADE,
+                             related_name='comment_news',
+                             verbose_name='Комментарий новости'
+                             )
+    text = models.TextField(max_length=300)
+    pub_data = models.DateTimeField('Дата публикации',
+                                    auto_now_add=True
+                                    )
+
+    def __str__(self):
+        return self.author
+
